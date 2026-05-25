61aa68e6de86bbe4db3497015c21e85fd2ec28ca
rayalsaw09 <rayalsaw@gmail.com>
2026-05-25 15:05:47 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,18 @@
-const CACHE = "shopchampion-v1";
-const ASSETS = [
-  "/ShopChampion-Admin.html",
-  "/ShopChampion-Display.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+const CACHE = "shopchampion-v2";
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
